8d04692089188d88c450a24c9c4f148326ea54d2
tasneem <tasneemn.mansour@gmail.com>
2025-07-09 10:12:00 +0300

    Add flutter_screenutil plugin for Responsive UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:data_app/screens/launch_screen.dart';
 import 'package:data_app/screens/login_screen.dart';
 import 'package:data_app/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -13,23 +15,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-
-        )
-      ),
-      initialRoute: '',
-      routes: {
-        '/launc_screen' : (context) =>  LaunchScreen(),
-        '/login_screen' : (context) =>  LoginScreen(),
-        '/register_screen' : (context) =>  RegisterScreen(),
-        '/home_screen' : (context) =>  HomeScreen(),
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      splitScreenMode: true,
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              titleTextStyle: GoogleFonts.montserrat(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.sp
+              ),
+              iconTheme: const IconThemeData(color: Colors.black),
+            ),
+          ),
+          initialRoute: '/launc_screen',
+          routes: {
+            '/launc_screen': (context) => LaunchScreen(),
+            '/login_screen': (context) => LoginScreen(),
+            '/register_screen': (context) => RegisterScreen(),
+            '/home_screen': (context) => HomeScreen(),
+          },
+        );
       },
     );
   }
 }
+// .sp for fonts // .w for width // .h for hieght
--- lib/screens/launch_screen.dart
+++ lib/screens/launch_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LaunchScreen extends StatefulWidget {
   const LaunchScreen({super.key});
@@ -18,9 +20,15 @@ class _LaunchScreenState extends State<LaunchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Text('Data App'),
-        ),
+      body: Center(
+          child: Text(
+              'Data App',
+              style: GoogleFonts.montserrat(
+                color: Colors.black,
+                fontSize: 27.sp,
+              ),
+          ),
+      ),
     );
   }
 }
